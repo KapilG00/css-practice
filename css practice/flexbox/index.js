eb2c7f0fad3def7e6e2e12a8fa81c41efc0e3e1d
Kapil Gupta <kapil.gupta4949@gmail.com>
2024-06-17 19:48:06 +0530

    flexbox

---
+++ css practice/flexbox/index.js
@@ -0,0 +1,14 @@
+// FlexBox ->
+
+/*
+    1. Flex-box is used to layout, align and good distribution of space  among items in a container,
+       even when their size is unknown and/or dynamic (thus the word “flex”).
+    2. Flex-box containers are Flexible/Dynamic in nature.
+    3. Flex-box is good for almost any design (Except where we need a complex GRID).
+    4. Main Component of FlexBox is 
+        4.1 Flex-box Parent (Container)
+        4.2 Flex-box Children (Items).
+
+
+    LEARN MORE: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+*/
